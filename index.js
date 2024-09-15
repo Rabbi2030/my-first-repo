8519f4ec18ba90cbb1ada3d380e5ccdba5164ab2
fozle.rabbi2030@gmail.comRabbi2030 <fozle.rabbi2030@gmail.com>
2024-09-16 00:22:26 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let a = 20;
+let b = 30;
+
+
+function addition(a, b){
+    return a + b;
+
+}
+
+let add  = addition(a, b)
+document.getElementById('demo').innerHTML = `there is additon value: ${add}`;
+
+
+document.getElementById('abc').innerHTML = 'amar shonar bnalga';
+
